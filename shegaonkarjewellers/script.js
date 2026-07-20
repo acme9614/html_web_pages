0cbf75dd94e65d4ef2788acdc61f118e1ec354ff
ajitmane1810 <ajitmane1810@gmail.com>
2026-07-20 18:46:03 +0530

    commit code for  banners padding from top to show all image

--- shegaonkarjewellers/script.js
+++ shegaonkarjewellers/script.js
@@ -52,7 +52,7 @@ window.onBannerImagesLoaded = function () {
 
     // Change margin-top when dynamic banners are loaded
     bannerSection.classList.remove("mt-[98px]");
-    bannerSection.classList.add("pt-[300px]");
+    bannerSection.classList.add("pt-[100px]");
 
     // Force Flutter banners to behave like desktop banners
     document
